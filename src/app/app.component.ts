2e730e5c2fc937de261827a3509a79f0d58051a9
Mohit Bhole <43913328+moiiiiit@users.noreply.github.com>
2021-08-11 15:46:35 -0500

    Added Main Navigator and Theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NbThemeService } from '@nebular/theme';
+import { NbComponentSize } from '@nebular/theme';
+import { HostListener } from "@angular/core";
+import { NbIconLibraries } from '@nebular/theme';
 
 @Component({
   selector: 'app-root',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-portfolio';
+  size: NbComponentSize = 'medium';
+  title = 'Mohit Bhole';
+  theme = '';
+  screenheight = 0;
+  screenwidth = 0;
+  constructor(private readonly themeService: NbThemeService) {   
+    this.themeService.onThemeChange()
+          .subscribe((theme: any) => {
+            this.theme = theme.name;
+          });
+    this.onResize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event?) {
+    this.screenheight = window.innerHeight;
+    this.screenwidth = window.innerWidth;
+  }
+  
+  themeSwitch(){
+    if (this.theme == "dark"){
+      this.themeService.changeTheme('default');
+    }
+    else{
+      this.themeService.changeTheme('dark');
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbUserModule ,NbIconModule , NbThemeModule, NbLayoutModule, NbActionsModule, NbButtonModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { ResumeComponent } from './resume/resume.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ResumeComponent, AboutComponent, HomeComponent, ProjectsComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbLayoutModule,
+    NbEvaIconsModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: 'resume', component: ResumeComponent },
+      { path: 'contact', component: AboutComponent },
+      { path: 'projects', component: ProjectsComponent },
+    ]),
+    NbActionsModule,
+    NbButtonModule,
+    NbIconModule,
+    NbUserModule,
+    NbThemeModule.forRoot({name:  'dark'}),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
